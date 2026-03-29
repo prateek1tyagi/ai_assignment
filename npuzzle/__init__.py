fbb64899e4d10b289fe72116af3fb5f54d5df8c1
prateek1tyagi <paritjain26@gmail.com>
2026-03-30 01:04:37 +0530

    Update __init__.py to include core and search functions
    Add core components and search algorithms to module exports.

--- npuzzle/__init__.py
+++ npuzzle/__init__.py
@@ -1,1 +1,15 @@
+from .core import GOAL_STATE, PuzzleState
+from .heuristics import linear_conflict, manhattan_distance
+from .search import SearchResult, solve_astar, solve_bfs, solve_idastar, solve_iddfs
 
+__all__ = [
+    "GOAL_STATE",
+    "PuzzleState",
+    "SearchResult",
+    "linear_conflict",
+    "manhattan_distance",
+    "solve_astar",
+    "solve_bfs",
+    "solve_idastar",
+    "solve_iddfs",
+]
